f0c9c59bf9781c4e40fd679ac0f8c1ce178f6cbb
DylanGodfrey <39961099+DylanGodfrey@users.noreply.github.com>
2022-10-03 10:11:06 -0400

    Final touch ups completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 searchInput = document.querySelector("#searchBar"); //text input from user
 searchButton = document.querySelector("#searchButton"); // search button
-
-searchInput.value = "inception"; //default
+recentSearchesEl = document.querySelector("#searches")
 
 function createQueryString(event) {
   event.preventDefault();
@@ -9,4 +8,37 @@ function createQueryString(event) {
   location.href = "./displayPage.html?title=" + queryString;
 }
 
+var recentSearches = [];
+
+if(localStorage["recentSearches"]) {
+  var ul = document.getElementById("list");
+  var li = document.createElement("li");
+
+  recentSearches = JSON.parse(localStorage["recentSearches"]);
+  
+  for (var i =0; i < recentSearches.length;i++) {
+    
+    var a = document.createElement("a");
+    
+    a.textContent = recentSearches[i];
+    a.setAttribute('href', "./displayPage.html?title=" +recentSearches[i]);
+
+    li.appendChild(a);
+
+    li.style.padding = "5px";
+    li.style.margin = "5px";
+    li.style.backgroundColor = "grey";
+
+    ul.appendChild(li);
+    li = document.createElement("li");
+  }
+}
+
 searchButton.addEventListener("click", createQueryString); //fetch imdb API data on button click
+
+searchInput.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById(searchButton).click();
+    }
+});
